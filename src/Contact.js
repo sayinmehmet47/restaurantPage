4a465df4da145b47f370bb16315a06a071e5cbd4
sayin47 <sayinmehmet47@gmail.com>
2021-06-09 19:53:57 +0300

    completed

--- src/Contact.js
+++ src/Contact.js
@@ -3,8 +3,34 @@ const pages = document.querySelector('#pages');
 const Contact = () => {
   const contact = document.createElement('div');
   contact.innerHTML = `
-        <h1>contact</h1>
-    
+
+        <div class="icon">
+        <span class="fa-stack fa-2x">
+        <i class="fas fa-circle fa-stack-2x"></i>
+        <i class="fas fa-map-marker fa-stack-1x fa-inverse"></i>
+        </span>
+        <span>Cheironos 10 Athens/Greece</span>
+        </div>
+        <div class="icon">
+        <span class="fa-stack fa-2x">
+        <i class="fas fa-circle fa-stack-2x"></i>
+        <i class="fas fa-envelope fa-stack-1x fa-inverse"></i>
+
+    </span>
+    <span>sayinmehmet47@gmail.com</span>
+
+        </div>
+ 
+    <div class="icon">
+    <span class="fa-stack fa-2x">
+    <i class="fas fa-circle fa-stack-2x"></i>
+    <i class="fas fa-phone fa-stack-1x fa-inverse"></i>
+    </span>
+    <span>+306984870208</span>
+
+    </div>
+
+ 
     
     `;
     contact.classList.add('contactContainer');
--- src/Menu.js
+++ src/Menu.js
@@ -7,24 +7,21 @@ const Menu=()=>{
     menu.innerHTML=`
     <div class="card">
     <img src="https://i4.hurimg.com/i/hurriyet/75/750x422/5d4015917152d80f80b3f75a.jpg" alt="Denim Jeans" style="width:100%">
-    <h1>Tailored Jeans</h1>
-    <p class="price">$19.99</p>
+    <h1>Alinazik</h1>
+    <p class="price">$6.99</p>
     <p>Some text about the jeans..</p>
-    <p><button>Add to Cart</button></p>
   </div>    
   <div class="card">
   <img src="https://iqhaber.com/upload/media/entries/2018-04/22/144-2-e9708934e22534bfe5e2f712be67e001.jpg" alt="Denim Jeans" style="width:100%">
-  <h1>Tailored Jeans</h1>
-  <p class="price">$19.99</p>
+  <h1>Pide</h1>
+  <p class="price">$6.99</p>
   <p>Some text about the jeans..</p>
-  <p><button>Add to Cart</button></p>
 </div>
 <div class="card">
 <img src="https://iqhaber.com/upload/media/entries/2018-04/22/144-1-e9708934e22534bfe5e2f712be67e001.jpg" alt="Denim Jeans" style="width:100%">
-<h1>Tailored Jeans</h1>
-  <p class="price">$19.99</p>
+<h1>Lahmacun</h1>
+  <p class="price">$5.99</p>
   <p>Some text about the jeans..</p>
-  <p><button>Add to Cart</button></p>
 </div>
     `
         menu.classList.add("menuContainer")
